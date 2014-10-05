3386dce86d83aecb5fd24d72c7f5c2198360e714
altunhasangmailcom <altunhasan@gmail.com>
2014-10-06 01:35:38 +0300

    greeting 2 also changed

--- project1/src/hello/world/Greeting2.java
+++ project1/src/hello/world/Greeting2.java
@@ -2,7 +2,7 @@ package hello.world;
 
 public class Greeting2 {
 	public static void main(String[] args) {
-		System.out.println("Greeting 2");
+		System.out.println("Greeting 2 changed by altunhasanmail");
 	}
 }
 
